5e4684ada968c2fc5efe172f35786b2e0ca33d17
AZUSIntel <AZUSIntel@DESKTOP-1D10ILG>
2024-10-19 15:32:13 +0200

    ajoute d'un phrase

--- src/Tp0.java
+++ src/Tp0.java
@@ -2,5 +2,8 @@
 public class Tp0 {
     public static void main(String[] args) {
     	
-System.out.println("Hello World");}
+System.out.println("Hello World");
+System.out.println("Je suis une version modifiée du code de l'exo 1"); 
+
+    }
 }
